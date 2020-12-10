4fd704b6733af0824f2f77e78c27cb08a57a1e34
dromero1 <romeromillandavidandres@gmail.com>
2020-12-10 17:55:08 -0500

    json read

--- GeoUrbanCOVID19/src/config/SourceFeatures.java
+++ GeoUrbanCOVID19/src/config/SourceFeatures.java
@@ -3,21 +3,6 @@ package config;
 public final class SourceFeatures {
 
 	/**
-	 * Policies database - type column
-	 */
-	public static final int POLICIES_TYPE_COLUMN = 0;
-
-	/**
-	 * Policies database - begin day column
-	 */
-	public static final int POLICIES_BEGIN_DAY_COLUMN = 1;
-
-	/**
-	 * Policies database - end day column
-	 */
-	public static final int POLICIES_END_DAY_COLUMN = 2;
-
-	/**
 	 * Communes database - id column
 	 */
 	public static final int COMMUNES_ID_COLUMN = 0;
